e28835abd74788c7951fdcf2592108b5b0d97c06
lex <josalexander25@gmail.com>
2025-10-21 17:10:27 -0500

    version 1.8

--- src/pages/Search-Page.tsx
+++ src/pages/Search-Page.tsx
@@ -1,7 +1,40 @@
+import Input from "../components/input";
+import * as React from "react"
+import { getPokemon } from "../services/pokeapi-service";
 
 const SearchPage = () => {
+  const [query, setQuery] = React.useState("");
+  const [pokemon, setPokemon] = React.useState(null);
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    getPokemon(query).then((data)=> setPokemon(data)).catch((error)=>console.log(error))
+  }
+
   return (
-   <h3>Search Page</h3>
+   <>
+   <>
+    <form onSubmit={handleSubmit}>
+      <Input
+        name="query"
+        placeholder="pokemon name"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button>Search</button>
+    </form>
+    {pokemon ? (
+      <div>
+        <h2>{pokemon.name}</h2>
+        <p>{pokemon.id}</p>
+        <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
+        </div>
+    ) : (
+      "Ready to search"
+    )}
+    </>
+   </>
   )
 }
 
